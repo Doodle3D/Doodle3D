4789a7389d28f19fef1a84889be6d955def38c8f
Rick Companje <rick@companje.nl>
2013-01-22 19:54:03 +0100

    updated submodules

---
+++ scripts/osx/Doodle3D-6x.sh
@@ -0,0 +1,11 @@
+SCRIPTDIR=$(dirname $0)
+cd $SCRIPTDIR
+
+open /Applications/Doodle3D.app --new --args loadSettings=1.ini
+open /Applications/Doodle3D.app --new --args loadSettings=2.ini
+open /Applications/Doodle3D.app --new --args loadSettings=3.ini
+open /Applications/Doodle3D.app --new --args loadSettings=4.ini
+open /Applications/Doodle3D.app --new --args loadSettings=5.ini
+open /Applications/Doodle3D.app --new --args loadSettings=6.ini
+
+sleep 2
